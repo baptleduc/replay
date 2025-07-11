f097493e64699a72c926f27842e9fdc4b3e8f916
Le Duc Léandre <leandre.leduc38@gmail.com>
2025-07-11 20:04:06 +0200

    feat(parsing): parse the CLI args using the clap library
    - Run and Record commands are handled
    - Error when no <COMMAND> entry
    - Options like show, delay, etc...

--- src/args.rs
+++ src/args.rs
@@ -3,3 +3,43 @@
 //! `Args` is the parsing module used by our main library
 //! It will ensure we get the correct args and then return
 //! a correct Structure to run the corresponding commands
+
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+pub struct CliParser {
+    #[command(subcommand)]
+    pub command: Option<CliCommand>,
+}
+
+#[derive(Subcommand)]
+pub enum CliCommand {
+    /// Run a replay on the specified session
+    Run {
+        session_name: String,
+
+        /// Show commands without executing them
+        #[arg(short, long)]
+        show: bool,
+
+        /// Delay (in milliseconds) between commands
+        #[arg(long, short, default_value_t = 0, value_name = "ms")]
+        delay: u64,
+
+        /// Start replay from command number <N>
+        #[arg(long, short, value_name = "N")]
+        from: Option<u64>,
+
+        /// Stop replay at command number N
+        #[arg(long, short, value_name = "N")]
+        to: Option<u64>,
+    },
+
+    /// Record a new session of shell commands
+    /// if a session name is provided, it will be used to label the recording
+    Record {
+        /// Optional session name
+        session_name: Option<String>,
+    },
+}
--- src/lib.rs
+++ src/lib.rs
@@ -11,26 +11,42 @@
 //! - [`args`] Defines the command-line interface using `clap`.
 //! - [`commands`] Contains implementations of all supported subcommands.
 //!
-//! ## Usage
-//!
-//! ```no_run
-//! fn main() -> anyhow::Result<()> {
-//!     replay::run()?;
-//!     Ok(())
-//! }
-//! ```
 //!
 //! ## Entry Point
 //!
 //! Use [`run`] to execute the CLI logic from a binary.
 
-use std::error::Error;
-
 pub mod args;
 pub mod commands;
 
+use args::{CliCommand, CliParser};
+use clap::Parser;
+use commands::{CommandRunner, record::RecordCommand, run::RunCommand};
+
 /// Entrypoint called by the binary.
-/// Parses CLI arguments and dispatches the appropriate command.
-pub fn run() -> Result<(), Box<dyn Error>> {
+/// Parses CLI arguments and run the appropriate command.
+pub fn run() -> Result<(), &'static str> {
+    // We parse the CLI using our CliParser
+    let cli_args = CliParser::parse();
+
+    let command = match cli_args.command {
+        Some(cmd) => cmd,
+        None => {
+            return Err("You didn't specify a command, perhaps you should try `--help`");
+        }
+    };
+
+    // Then we launch the corresponding command
+    let cmd: Box<dyn CommandRunner> = match command {
+        CliCommand::Run {
+            session_name,
+            show,
+            delay,
+            from,
+            to,
+        } => Box::new(RunCommand::new(session_name, show, delay, from, to)),
+        CliCommand::Record { session_name } => Box::new(RecordCommand::new(session_name)),
+    };
+    cmd.run()?;
     Ok(())
 }
