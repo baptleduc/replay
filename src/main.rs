83de9c2e85e7aa91998eed3b82ebdaf924bc1fc9
Le Duc Léandre <leandre.leduc38@gmail.com>
2025-08-01 19:23:54 +0200

    refactor(error): create ReplayError type error to handle possible PTY/bash errors during `run` from record
    - Add a disabling raw mode statement into main when errors occured
    - Handle every errors from the record `run` function instead or just
      unwrapping them

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,16 @@
 // It delegates execution to the `replay` crate, which handles
 // Argument parsing, command dispatching and core logic.
 
+use crossterm::terminal;
 use replay::run;
 use std::{env, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     if let Err(err) = run(&args) {
+        if let Err(e) = terminal::disable_raw_mode() {
+            eprintln!("Impossible to quit the raw mode: {e}")
+        }
         eprintln!("{}", err);
         process::exit(1)
     };
