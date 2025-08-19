d1b16ab4c407b1f76b84b9ab4eddfa21de5dc032
Baptiste Le Duc <baptiste.leduc38@gmail.com>
2025-08-19 10:08:24 +0200

    chore(json): remove useless if statement to test if a dir exist

--- src/paths.rs
+++ src/paths.rs
@@ -10,17 +10,13 @@ pub fn get_replay_dir() -> PathBuf {
     #[cfg(test)]
     let dir = temp_dir().join(".replay");
 
-    if !dir.exists() {
-        fs::create_dir_all(&dir).expect("Failed to create .replay directory");
-    }
+    fs::create_dir_all(&dir).expect("Failed to create .replay directory");
     dir
 }
 
 pub fn get_sessions_dir() -> PathBuf {
     let dir = get_replay_dir().join("sessions");
 
-    if !dir.exists() {
-        fs::create_dir_all(&dir).expect("Failed to create sessions directory");
-    }
+    fs::create_dir_all(&dir).expect("Failed to create sessions directory");
     dir
 }
