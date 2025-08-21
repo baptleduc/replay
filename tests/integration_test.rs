36b16482b6f141ee9c9f420d6244bfc6d839b6ec
Le Duc Léandre <leandre.leduc38@gmail.com>
2025-08-21 16:07:05 +0200

    test(refactor): refactored the test with new commentary and regex match

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -7,48 +7,33 @@ use uuid::Uuid;
 // Here we will implement the integrations_tests
 #[test]
 #[serial]
-fn test_list_after_pop_session() {
-    let session_name = &Uuid::new_v4().to_string()[..10];
-    let session = Session::new(Some(session_name.to_string())).unwrap();
-    session.save_session(true).unwrap();
-
-    Command::cargo_bin("replay")
-        .unwrap()
-        .arg("list")
-        .assert()
-        .stdout(predicates::str::contains(session_name));
-
-    Command::cargo_bin("replay")
-        .unwrap()
-        .arg("pop")
-        .assert()
-        .success();
-
-    Command::cargo_bin("replay")
-        .unwrap()
-        .arg("list")
-        .assert()
-        .stdout(predicates::str::contains(session_name).not());
-}
-
-#[test]
-#[serial]
 fn test_list_after_drop_session() {
-    let session_name1 = &Uuid::new_v4().to_string()[..10];
-    let session_name2 = &Uuid::new_v4().to_string()[..10];
+    // We use the user's real session directory for testing,
+    // so to avoid collisions with existing sessions,
+    // we generate unique random UUID-based descriptions.
+    let session_desc1 = &Uuid::new_v4().to_string()[..10];
+    let session_desc2 = &Uuid::new_v4().to_string()[..10];
 
-    let session1 = Session::new(Some(session_name1.to_string())).unwrap();
+    let session1 = Session::new(Some(session_desc1.to_string())).unwrap();
     session1.save_session(true).unwrap();
 
-    let session2 = Session::new(Some(session_name2.to_string())).unwrap();
+    let session2 = Session::new(Some(session_desc2.to_string())).unwrap();
     session2.save_session(true).unwrap();
 
+    let pattern = format!(
+        r"(?m)^replay@\{{0\}}: .*, message: {}$",
+        // Since we match with regex and UUID contains `-`, need to escape it
+        session_desc2.replace("-", r"\-")
+    );
+    // Verify both sessions appear in `list` output
     Command::cargo_bin("replay")
         .unwrap()
         .arg("list")
         .assert()
         .stdout(
-            predicates::str::contains(session_name1).and(predicates::str::contains(session_name2)),
+            predicates::str::contains(session_desc1)
+                .and(predicates::str::contains(session_desc2))
+                .and(predicates::str::is_match(pattern).unwrap()),
         );
 
     Command::cargo_bin("replay")
@@ -58,16 +43,16 @@ fn test_list_after_drop_session() {
         .assert()
         .success();
 
+    // Ensure the first session is no longer listed
     Command::cargo_bin("replay")
         .unwrap()
         .arg("list")
         .assert()
-        .stdout(predicates::str::contains(session_name1).not());
+        .stdout(predicates::str::contains(session_desc1).not());
 
     Command::cargo_bin("replay")
         .unwrap()
         .arg("drop")
-        .arg("replay@{0}")
         .assert()
         .success();
 
@@ -75,5 +60,5 @@ fn test_list_after_drop_session() {
         .unwrap()
         .arg("list")
         .assert()
-        .stdout(predicates::str::contains(session_name2).not());
+        .stdout(predicates::str::contains(session_desc2).not());
 }
